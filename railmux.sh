97494a2c8a9ad39738ef5c2b092d44aeab1f1864
Dan Berger <dsberger@gmail.com>
2015-04-04 23:01:58 -0700

    Revisions to the Rails Tmux script

--- railmux.sh
+++ railmux.sh
@@ -3,6 +3,14 @@ SESSION=${PWD##*/}
 
 tmux -2 new-session -d -s $SESSION
 
+# Create small left pane and open guard
+tmux split-window -h
+tmux send-keys "bundle exec guard" C-m
+
+# Return to left pane and run a git status
+tmux select-pane -L
+tmux send-keys "git status" C-m
+
 # Open the Rails server
 tmux new-window -t $SESSION:1 -n 'SERVER'
 tmux send-keys "rails s" C-m
@@ -11,16 +19,9 @@ tmux send-keys "rails s" C-m
 tmux new-window -t $SESSION:2 -n 'CONSOLE'
 tmux send-keys "rails c" C-m
 
-# Open spec directory with guard running
-tmux new-window -t $SESSION:3 -n 'SPEC'
-tmux split-window -h
-tmux resize-pane -R 20
-tmux send-keys "bundle exec guard" C-m
-tmux select-pane -L
-tmux send-keys "cd spec" C-m
-
-# Set default window
+# Return to primary window
 tmux select-window -t $SESSION:0
+tmux rename-window 'WORKSPACE'
 
 # Attach to session
 tmux -2 attach-session -t $SESSION
